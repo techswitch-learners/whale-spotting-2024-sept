8c0bc4185eac0f2d33752d8f9fab2e62cb45148b
Dorothy Wong <dorothyynwong@gmail.com>
2024-09-03 11:36:09 +0100

    added properties to User.cs, added migrations for changes of Users database table AspNetUsers

--- backend/Models/Data/User.cs
+++ backend/Models/Data/User.cs
@@ -1,5 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace WhaleSpotting.Models.Data;
 
-public class User : IdentityUser<int> { }
+public class User : IdentityUser<int>
+{
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public int TotalPointsEarned { get; set; }
+
+    public string? AboutMe { get; set; }
+
+    public bool IsSuspended { get; set; }
+}
