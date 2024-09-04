726dd93756b033056f9c62eeaebd71e23c54d722
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-04 10:41:14 +0100

    Added species table and species name as index + migration implemented successfully
    Co-authored-by: Robert De Leon <bobbybwoy@users.noreply.github.com>

--- backend/Models/Data/Species.cs
+++ backend/Models/Data/Species.cs
@@ -3,8 +3,6 @@ namespace WhaleSpotting.Models.Data;
 public class Species
 {
     public int SpeciesId { get; set; }
-
-    //TO DO: Create index for species name
     public string SpeciesName { get; set; }
     public string ExampleLink { get; set; }
     public string TailPictureLink { get; set; }
