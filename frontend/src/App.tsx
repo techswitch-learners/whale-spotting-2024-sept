d9b5273e760f84f659f5bea5de63d79b492ab656
Amy <amysun.uk@gmail.com>
2024-09-04 13:50:16 +0100

    set up header component with bootstrap styling, added logo to and changed title in index.html, added empty record page and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Explore from "./pages/Explore"
 import Home from "./pages/Home"
 import "./App.scss"
+import Record from "./pages/Record"
+import Header from "./Header/Header"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Explore" element={<Explore />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/record" element={<Record />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Record.tsx
@@ -0,0 +1,5 @@
+const Record = () => {
+  return <h1>Record</h1>
+}
+
+export default Record
