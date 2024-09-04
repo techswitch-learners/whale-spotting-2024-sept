6f80d748683b1175c3d56dbf67164de5cf467a5b
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-04 11:33:10 +0100

    Set default values for Species.cs
    Co-authored-by: Robert De Leon <bobbybwoy@users.noreply.github.com>

--- backend/Models/Data/Species.cs
+++ backend/Models/Data/Species.cs
@@ -3,10 +3,10 @@ namespace WhaleSpotting.Models.Data;
 public class Species
 {
     public int SpeciesId { get; set; }
-    public string SpeciesName { get; set; }
-    public string ExampleLink { get; set; }
-    public string TailPictureLink { get; set; }
-    public string WikiLink { get; set; }
-    public int TotalSightings { get; set; }
+    public string SpeciesName { get; set; } = string.Empty;
+    public string ExampleLink { get; set; } = string.Empty;
+    public string TailPictureLink { get; set; } = string.Empty;
+    public string WikiLink { get; set; } = string.Empty;
+    public int TotalSightings { get; set; } = 0;
 
 }
