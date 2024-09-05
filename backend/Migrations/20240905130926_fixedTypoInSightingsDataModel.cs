ef34e6a135ebb52619e28db440d50f65c507ae09
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-05 14:10:53 +0100

    fixed typo on line 14 of the Sightings.cs data model, from "Lattitude" to "Latitude"

---
+++ backend/Migrations/20240905130926_fixedTypoInSightingsDataModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhaleSpotting.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedTypoInSightingsDataModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lattitude",
+                table: "Sightings",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Sightings",
+                newName: "Lattitude");
+        }
+    }
+}
