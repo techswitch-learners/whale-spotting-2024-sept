c412c1303b505d8fdc12fc885e2e7897fff0dfe3
Natasha Buckham <natashaisobel@gmail.com>
2024-09-10 10:28:07 +0100

    Adds comments to backendClient.ts to explain how to pass the JWT token in future fetch requests

--- frontend/src/api/backendClient.ts
+++ frontend/src/api/backendClient.ts
@@ -23,3 +23,11 @@ export const registerUser = async (username: string, password: string) => {
     }),
   })
 }
+
+// to add the JWT token as a header to fetch requests which access protected endpoints do the following:
+// In the .tsx file where the fetch request is being called:
+// 1) import the login context to access the value of the JWT token
+// 2) pass the token as a prop called 'header' in the method which calls the fetch request
+// In this file:
+// 1) add 'header: string' as a prop to the method
+// 2) add "Authorization": `Bearer ${header}` to the list of headers
