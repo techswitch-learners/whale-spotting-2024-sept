347424a814834d54e1fd8c97813da4974ed26d65
Amy <amysun.uk@gmail.com>
2024-09-11 11:31:09 +0100

    fixed grammarchanged cors and audience port to 3000, changed name of test in app.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import App from "./App"
 
-test("renders log in link title", () => {
+test("renders log in link", () => {
   render(<App />)
   const linkElement = screen.getAllByText(/log in/i)
   expect(linkElement[0]).toBeInTheDocument()
