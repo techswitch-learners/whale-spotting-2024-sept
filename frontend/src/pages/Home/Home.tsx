f08c7a9a3288c17c12f52700a488bcc4f1ff07e8
Dorothy Wong <dorothyynwong@gmail.com>
2024-09-12 10:55:23 +0100

    working on the homepage layout for both web browser and mobile devices Co-authored-by: Claudia Giancola <claudiaGiancola@users.noreply.github.com>

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,34 +3,39 @@ import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
 
+  const logo = require("./../../images/Whale3logo.png");
+
   const navigate = useNavigate()
 
   const handleClick = (event: { currentTarget: { id: string; }; }) => {
     const buttonId = event.currentTarget.id;
-    switch (buttonId)
-    {
+    switch (buttonId) {
       case "register-sighting-button":
         navigate("/");
         break;
       case "explore-button":
-          navigate("/explore");
-          break;
+        navigate("/explore");
+        break;
       default:
-          break;
+        break;
     }
   }
 
   return (
     <>
-    <div className="background-container">
-      {/* <img id="homepage-top-img" src="https://hips.hearstapps.com/hmg-prod/images/breaching-humpback-whale-cabo-san-lucas-baja-california-1531325454.jpg" /> */}
-        <button id="register-sighting-button" data-testid="register-sighting-button" className="btn btn-primary btn-lg" style={{ width: "300px", margin: "10px" }} onClick={handleClick}>
-          Register a Sighting
-        </button>
-        <button id="explore-button" data-testid="explore-button" className="btn btn-primary btn-lg" style={{ width: "300px", margin: "10px" }} onClick={handleClick}>
-          Explore
-        </button>
-    </div>
+      <div className="background-container">
+        <div className="logo">
+          <img src={logo} alt="Whale Whale Whale logo" width="200" />
+        </div>
+        <div className="top-buttons">
+          <button id="register-sighting-button" data-testid="register-sighting-button" className="btn btn-primary btn-lg" style={{ width: "30vw", height: "8vh", margin: "10px" }} onClick={handleClick}>
+            Submit
+          </button>
+          <button id="explore-button" data-testid="explore-button" className="btn btn-primary btn-lg" style={{ width: "30vw", height: "8vh", margin: "10px" }} onClick={handleClick}>
+            Explore
+          </button>
+        </div>
+      </div>
     </>
   )
 }
