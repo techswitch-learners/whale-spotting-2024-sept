3368de467ce42517c32dc2eb9f31d3f4425b22b4
Natasha Buckham <natashaisobel@gmail.com>
2024-09-13 11:01:44 +0100

    Adds /users/profile endpoint and creates ProfileResponse model

---
+++ backend/Models/Response/ProfileResponse.cs
@@ -0,0 +1,12 @@
+namespace WhaleSpotting.Models.Response;
+
+public class ProfileResponse
+{
+    public string? UserName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+    public int TotalPointsEarned { get; set; }
+    public string? AboutMe { get; set; }
+}
