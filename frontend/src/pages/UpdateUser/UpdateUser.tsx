3f3319a52954ca1eb64e30452952870259dcb084
Natasha Buckham <natashaisobel@gmail.com>
2024-09-17 15:57:19 +0100

    Adds further styling

--- frontend/src/pages/UpdateUser/UpdateUser.tsx
+++ frontend/src/pages/UpdateUser/UpdateUser.tsx
@@ -8,14 +8,13 @@ export function UpdateUser(): JSX.Element {
   const loginContext = useContext(LoginContext)
   const location = useLocation()
   const userData = location.state
-  
+
   const [firstname, setFirstName] = useState(userData.firstName)
   const [lastname, setLastName] = useState(userData.lastName)
   const [aboutme, setAboutMe] = useState(userData.aboutMe)
   const [error, setError] = useState(null)
 
   const navigate = useNavigate()
-  
 
   function tryUpdate(event: FormEvent) {
     event.preventDefault()
@@ -37,65 +36,64 @@ export function UpdateUser(): JSX.Element {
   }
 
   return (
-    <div className="container col-10 col-md-6 pt-3 mx-auto text-center">
+    <div className="container col-10 col-md-6 pt-4 mx-auto text-center">
       <div className="row">
         <h1 className="title">Update My Details</h1>
       </div>
       <div className="row justify-content-center">
-      <form className="form" onSubmit={tryUpdate}>
-        <div className="form-group row">
-          <label htmlFor="firstName" className="col-sm-2 col-lg-4 col-form-label">
-            First Name
-          </label>
-          <div className="col-sm-3 col-lg-6">
-            <input
-              id="firstName"
-              className="form-control"
-              type={"text"}
-              value={firstname}
-              onChange={(event) => setFirstName(event.target.value)}
-            />
+        <form className="form justify-content-center" onSubmit={tryUpdate}>
+          <div className="form-group row justify-content-center my-4">
+            <label htmlFor="firstName" className="col-sm-2 col-lg-4 col-form-label">
+              First Name
+            </label>
+            <div className="col-sm-3 col-lg-6">
+              <input
+                id="firstName"
+                className="form-control"
+                type={"text"}
+                value={firstname}
+                onChange={(event) => setFirstName(event.target.value)}
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="lastName" className="col-sm-2 col-form-label">
-            Last Name
-          </label>
-          <div className="col-sm-3">
-            <input
-              id="lastName"
-              className="form-control"
-              type={"text"}
-              value={lastname}
-              onChange={(event) => setLastName(event.target.value)}
-            />
+          <div className="form-group row justify-content-center my-4">
+            <label htmlFor="lastName" className="col-sm-2 col-lg-4 col-form-label">
+              Last Name
+            </label>
+            <div className="col-sm-3 col-lg-6">
+              <input
+                id="lastName"
+                className="form-control"
+                type={"text"}
+                value={lastname}
+                onChange={(event) => setLastName(event.target.value)}
+              />
+            </div>
           </div>
-        </div>
-       
 
-        <div className="form-group row">
-          <label htmlFor="aboutMe" className="col-sm-2 col-form-label">
-            About Me
-          </label>
-          <div className="col-sm-3">
-            <textarea
-              id="aboutMe"
-              className="form-control"
-              value={aboutme}
-              onChange={(event) => setAboutMe(event.target.value)}
-            ></textarea>
+          <div className="form-group row justify-content-center my-4">
+            <label htmlFor="aboutMe" className="col-sm-2 col-lg-4 col-form-label">
+              About Me
+            </label>
+            <div className="col-sm-3 col-lg-6">
+              <textarea
+                id="aboutMe"
+                className="form-control"
+                value={aboutme}
+                onChange={(event) => setAboutMe(event.target.value)}
+              ></textarea>
+            </div>
           </div>
-        </div>
 
-        <div className="form-group row">
-          <div className="col-sm-10">
-            <button className="btn btn-outline-success px-4" style={{ width: "200px" }} onClick={tryUpdate}>
-              Update
-            </button>
+          <div className="form-group row justify-content-center">
+            <div className="col-sm-10 my-3">
+              <button className="btn btn-outline-success px-4" style={{ width: "200px" }} onClick={tryUpdate}>
+                Update
+              </button>
+            </div>
           </div>
-        </div>
-        {error && <p style={{ color: "red" }}> {error}</p>}
-      </form>
+          {error && <p style={{ color: "red" }}> {error}</p>}
+        </form>
       </div>
     </div>
   )
