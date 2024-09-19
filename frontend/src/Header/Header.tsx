bc9f3144f76db51703225c0fb61e0fc9bcb0e2aa
Natasha Buckham <natashaisobel@gmail.com>
2024-09-19 15:36:00 +0100

    change a tag to React Link in Header, fix Profile bug

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,6 +1,7 @@
 import "./Header.scss"
 import "../App.scss"
 import NavigationLinks from "../NavigationLinks/NavigationLinks"
+import { Link } from "react-router-dom"
 
 const Header: React.FC = () => {
   const logo = require("../images/Whale3logo.png")
@@ -9,9 +10,12 @@ const Header: React.FC = () => {
     <div className="show-header">
       <nav className="navbar navbar-expand-lg fixed-top custom-navbar">
         <div className="container-fluid d-flex justify-content-between align-items-center">
-          <a className="navbar-brand" href="/">
+          {/* <a className="navbar-brand" href="/">
             <img src={logo} alt="Whale Whale Whale logo" width="200" />
-          </a>
+          </a> */}
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="Whale Whale Whale logo" width="200" />
+          </Link>
           <NavigationLinks />
         </div>
       </nav>
