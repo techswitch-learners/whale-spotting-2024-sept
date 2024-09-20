97fd08d1c054397598497dd227151ba4319216a5
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-20 10:06:45 +0100

    merged Gallery with Sightings gallery

--- frontend/src/Components/SightingsGallery/SightingsGallery.tsx
+++ frontend/src/Components/SightingsGallery/SightingsGallery.tsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect, useState } from 'react';
-import { LoginContext } from '../../Components/LoginManager/LoginManager';
-import { getSightings } from '../../api/backendClient';
-import { SightingType } from '../../pages/Explore/Explore';
+import { useContext, useEffect, useState } from "react"
+import { LoginContext } from "../../Components/LoginManager/LoginManager"
+import { getSightings } from "../../api/backendClient"
+import { SightingType } from "../../pages/Explore/Explore"
 import "./SightingsGallery.scss"
+import { Link } from "react-router-dom"
 
 const formatDate = (dateTime: Date) => {
     return new Date(dateTime).toLocaleString("en-GB", { timeZone: "UTC" })
@@ -38,25 +39,25 @@ function SightingsGallery(): JSX.Element {
             <div className="gallery-container">
                 {sightings.map(sighting => (
                     <div className="card" style={{ width: "20rem" }}>
-                        <img className="card-img-top" src={sighting.photoUrl} alt="Card image cap" style={{ maxHeight: "12rem", objectFit: "cover" }} />
-                        <div className="card-username-species">
-                            <h6 className="username">{sighting.username}</h6>
-                            <p className="species-name">{sighting.speciesName}</p>
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">{sighting.description}</p>
-                        </div>
-                        <div className="card-img-overlay">
-                            <p>{sighting.id}</p>
-                            <p>{formatDate(sighting.dateTime)}</p>
-                        </div>
+                        <Link className='link' to={`/sightings/${sighting.id}`}>
+                            <img className="card-img-top" src={sighting.photoUrl} alt="Card image cap" style={{ maxHeight: "12rem", objectFit: "cover" }} />
+                            <div className="card-username-species">
+                                <h6 className="username">{sighting.username}</h6>
+                                <p className="species-name">{sighting.speciesName}</p>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">{sighting.description}</p>
+                            </div>
+                            <div className="card-img-overlay">
+                                <p>{sighting.id}</p>
+                                <p>{formatDate(sighting.dateTime)}</p>
+                            </div>
+                        </Link>
                     </div>
-
                 ))}
             </div>
         </div>
     )
-
 }
 
 export default SightingsGallery
