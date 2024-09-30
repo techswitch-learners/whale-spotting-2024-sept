51567d0d639992e49170b52169175a99e98c11ff
Robert De Leon <68812513+bobbybwoy@users.noreply.github.com>
2024-09-30 16:39:50 +0100

    Implemented delete user by admin

--- backend/Controllers/AdminController.cs
+++ backend/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WhaleSpotting.Models.Data;
 using WhaleSpotting.Services;
 
 namespace WhaleSpotting.Controllers;
@@ -7,9 +8,10 @@ namespace WhaleSpotting.Controllers;
 [ApiController]
 [Authorize(Roles = "Admin")]
 [Route("/admin")]
-public class AdminController(ISightingsService sightingsService) : Controller
+public class AdminController(ISightingsService sightingsService, IUserService userService) : Controller
 {
     private readonly ISightingsService _sightingsService = sightingsService;
+    private readonly IUserService _userService = userService;
 
     [HttpPut("approve/sighting={sightingId}")]
     public async Task<IActionResult> ApproveSighting([FromRoute] int sightingId)
@@ -18,6 +20,25 @@ public class AdminController(ISightingsService sightingsService) : Controller
         {
             await _sightingsService.ApproveSighting(sightingId);
             return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("users/delete/{username}")]
+    public async Task<IActionResult> DeleteUser([FromRoute] string username)
+    {
+        try
+        {
+            User user = await _userService.FindByName(username);
+            if (user is null) {
+                return BadRequest("User does not exist");
+            }
+
+            await _userService.Delete(user);
+            return NoContent();
         }
         catch (Exception e)
         {
